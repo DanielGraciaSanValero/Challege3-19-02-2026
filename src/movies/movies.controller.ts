6c04640ef264e4b11dd4869edaf11ea5068cfc68
DanielGraciaSanValero <a29340@svalero.com>
2026-02-19 12:54:37 +0100

    Fix: gestionando error en bootstrap para pasar lint

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,8 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { MoviesService } from './movies.service';
-import { ApiTags, ApiOkResponse } from '@nestjs/swagger';
+import { ApiTags } from '@nestjs/swagger';
 import { MovieDto } from './dto/create-movie.dto'; // Ajusta la ruta a tu carpeta DTO
-import { Movie } from './entities/movie.entity'; // O la interfaz que uses
 /*import { Movie } from './movies.entity';*/
 
 @ApiTags('movies')
